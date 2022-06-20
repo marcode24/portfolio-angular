11d24c6181bf5bdb24c9595260d41021241dc5fb
marco cruz <marco24cruz08@gmail.com>
2022-06-19 23:02:11 -0500

    :sparkles: dark mode added

--- src/app/core/components/navigation/navigation.component.ts
+++ src/app/core/components/navigation/navigation.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navigation',
@@ -6,10 +6,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navigation.component.css']
 })
 export class NavigationComponent implements OnInit {
+  private darkThemeName: string = 'dark-theme';
+  public isDarkTheme: boolean;
+  public activeSection: string;
 
-  constructor() { }
+  constructor() {
+    this.isDarkTheme = document.body.classList.contains(this.darkThemeName);
+  }
 
   ngOnInit(): void {
+  }
+
+  setTheme() {
+    document.body.classList.toggle(this.darkThemeName);
+    this.isDarkTheme = document.body.classList.contains(this.darkThemeName);
+  }
+
+  @HostListener('window:scroll', ['$event']) onWindowScroll(event: Event) {
+    const sections = document.querySelectorAll("section");
+    const navLi = document.querySelectorAll("li");
+    let current: any = '';
+    sections.forEach((section) => {
+      const sectionTop = section.offsetTop;
+      if (scrollY >= sectionTop - 60) {
+        current = section.getAttribute("id");
+      }
+    });
+    navLi.forEach((li) => {
+      li.classList.remove("active");
+      if (li.classList.contains(current)) {
+        li.classList.add("active");
+      }
+    });
   }
 
 }
