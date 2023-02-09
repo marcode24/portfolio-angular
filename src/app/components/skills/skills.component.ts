0aaa6c697f384461e388dbbc48ade122b60e2b6a
marco cruz <marco24cruz08@gmail.com>
2023-02-08 20:32:44 -0600

    :lipstick: fix skills section responsive

--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -12,6 +12,7 @@ export class SkillsComponent implements OnInit {
     { name: 'CSS 3', imagePath: 'assets/images/css.png', alt: 'css' },
     { name: 'Javascript', imagePath: 'assets/images/js.png', alt: 'js' },
     { name: 'Typescript', imagePath: 'assets/images/ts.png', alt: 'ts' },
+    { name: 'SASS', imagePath: 'assets/images/sass.png', alt: 'scss' },
     { name: 'Angular', imagePath: 'assets/images/angular.png', alt: 'angular' },
     { name: 'Bootstrap', imagePath: 'assets/images/bootstrap.png', alt: 'bootstrap' },
   ];
@@ -20,12 +21,19 @@ export class SkillsComponent implements OnInit {
     { name: 'MongoDB', imagePath: 'assets/images/mongo.png', alt: 'mongodb' },
     { name: 'MySql', imagePath: 'assets/images/mysql.png', alt: 'mysql' },
     { name: 'Nestjs', imagePath: 'assets/images/nestjs.png', alt: 'nestjs' },
+    { name: 'Docker', imagePath: 'assets/images/docker.png', alt: 'docker' },
   ];
   public cardsDataOthers: ICard[] = [
     { name: 'Git', imagePath: 'assets/images/git.png', alt: 'git' },
-    // { name: 'GitHub', imagePath: 'assets/images/github.png', alt: 'github' },
+    { name: 'Azure', imagePath: 'assets/images/azure.png', alt: 'azure' },
     { name: 'Figma', imagePath: 'assets/images/figma.png', alt: 'figma' },
     { name: 'Postman', imagePath: 'assets/images/postman.png', alt: 'postman' },
+    { name: 'Jest', imagePath: 'assets/images/jest.png', alt: 'jest' },
+    { name: 'Jasmine', imagePath: 'assets/images/jasmine.png', alt: 'jasmine' },
+  ];
+  public cardsDataLearning: ICard[] = [
+    { name: 'Vue', imagePath: 'assets/images/vue.png', alt: 'vue' },
+    { name: 'Tailwind', imagePath: 'assets/images/tailwind.png', alt: 'tailwind' },
   ];
 
   constructor() { }
